53d089c0bea8625328050d381af4376e271908c6
RoswelHelkaiser <144640067+RoswelHelkaiser@users.noreply.github.com>
2024-04-04 11:12:14 +0900

    Add Animation
    プレイヤー画像を差し替え、アニメーションを追加しました。

--- Src/Scene/ScenePlay.h
+++ Src/Scene/ScenePlay.h
@@ -1,19 +1,60 @@
 #pragma once
 
-#define PLAYER_PATH		("Data/PlayImage/Player.png")	//プレイヤー画像(仮)のパス
+#define PLAYER_PATH		("Data/PlayImage/runBoy/runBoy1.png")	//プレイヤー画像のパス
+
+//プレイヤー画像の種類
+enum PLAYER_IMAGE
+{
+	PLAYER_1 = 0,	//runBoy1
+	PLAYER_2,		//runBoy2
+	PLAYER_3,		//runBoy3
+	PLAYER_4,		//runBoy4
+	PLAYER_5,		//runBoy5
+	PLAYER_6,		//runBoy6
+	PLAYER_7,		//runBoy7
+	PLAYER_8,		//runBoy8
+	PLAYER_9,		//runBoy9
+	PLAYER_10,		//runBoy10
+
+	PLAYER_NUM,
+};
+
+//プレイヤー画像のパス
+const char PlayerImage_Path[PLAYER_NUM][100] =
+{
+	"Data/PlayImage/runBoy/runBoy1.png",	//runBoy1
+	"Data/PlayImage/runBoy/runBoy2.png",	//runBoy2
+	"Data/PlayImage/runBoy/runBoy3.png",	//runBoy3
+	"Data/PlayImage/runBoy/runBoy4.png",	//runBoy4
+	"Data/PlayImage/runBoy/runBoy5.png",	//runBoy5
+	"Data/PlayImage/runBoy/runBoy6.png",	//runBoy6
+	"Data/PlayImage/runBoy/runBoy7.png",	//runBoy7
+	"Data/PlayImage/runBoy/runBoy8.png",	//runBoy8
+	"Data/PlayImage/runBoy/runBoy9.png",	//runBoy9
+	"Data/PlayImage/runBoy/runBoy10.png",	//runBoy10
+};
 
 //プレイヤークラス
 class Player
 {
 	private:	//メンバ変数一覧
-		int PlayerHandle;	//画像ハンドル
-		float PosX, PosY;	//X座標,Y座標
-		float MoveSpeed;	//移動速度
+		int PlayerHandle[PLAYER_NUM];	//画像ハンドル
+		int HandleIndex;				//画像ハンドルの添え字用変数
+		int AnimeFrame;					//画像の再生時間
+		bool isReverse;					//アニメーションの逆再生フラグ
+		float PosX, PosY;				//X座標,Y座標
+		float MoveSpeed;				//移動速度
 
 	public:		//メソッド一覧
 		Player()	//コンストラクタ(全ての情報をリセット)
 		{
-			PlayerHandle = 0;
+			for (int i = 0; i < PLAYER_NUM; i++)
+			{
+				PlayerHandle[i] = 0;
+			}
+			HandleIndex = 0;
+			AnimeFrame = 0;
+			isReverse = false;
 			PosX = 0.0f;
 			PosY = 0.0f;
 			MoveSpeed = 0.0f;
@@ -21,13 +62,46 @@ class Player
 
 		void InitPlayer()	//プレイヤーの初期化
 		{
-			PlayerHandle = LoadGraph(PLAYER_PATH);	//画像の読み込み
-			PosX = 320.0f;		//初期X座標を設定
-			PosY = 400.0f;		//初期Y座標を設定
-			MoveSpeed = 3.0f;	//初期移動速度を設定
+			//画像の読み込み
+			LoadPlayer(PLAYER_1);	//runBoy1
+			LoadPlayer(PLAYER_2);	//runBoy2
+			LoadPlayer(PLAYER_3);	//runBoy3
+			LoadPlayer(PLAYER_4);	//runBoy4
+			LoadPlayer(PLAYER_5);	//runBoy5
+			LoadPlayer(PLAYER_6);	//runBoy6
+			LoadPlayer(PLAYER_7);	//runBoy7
+			LoadPlayer(PLAYER_8);	//runBoy8
+			LoadPlayer(PLAYER_9);	//runBoy9
+			LoadPlayer(PLAYER_10);	//runBoy10
+			PosX = 640.0f;			//初期X座標を設定
+			PosY = 550.0f;			//初期Y座標を設定
+			MoveSpeed = 3.0f;		//初期移動速度を設定
 		}
 
 		void StepPlayer()	//プレイヤー通常処理
+		{
+			MovePlayer();	//プレイヤーの移動処理
+
+			ChangePlayerImage();	//	プレイヤーの画像を変える
+		}
+
+		void DrawPlayer()	//プレイヤー描画処理
+		{
+			DrawRotaGraph((int)PosX, (int)PosY, 1.0f, 0.0f, PlayerHandle[HandleIndex], true);
+		}
+
+		void FinPlayer()	//プレイヤーの後処理
+		{
+			//画像を破棄
+			DeleteGraph(PlayerHandle[0]);
+		}
+
+		void LoadPlayer(PLAYER_IMAGE type)	//プレイヤー画像の読み込み
+		{
+			PlayerHandle[type] = LoadGraph(PlayerImage_Path[type]);
+		}
+
+		void MovePlayer()	//プレイヤーの移動処理
 		{
 			//Dキーまたは右矢印キーを押している間
 			if (IsKeyKeep(KEY_INPUT_D) || IsKeyKeep(KEY_INPUT_RIGHT))
@@ -36,10 +110,10 @@ class Player
 				PosX += MoveSpeed;
 
 				//画面端まで到達したら
-				if (PosX >= 624.0f)
+				if (PosX >= 1248.0f)
 				{
 					//それ以上右に行けないようにする
-					PosX = 624.0f;
+					PosX = 1248.0f;
 				}
 			}
 
@@ -50,23 +124,49 @@ class Player
 				PosX -= MoveSpeed;
 
 				//画面端まで到達したら
-				if (PosX <= 16.0f)
+				if (PosX <= 32.0f)
 				{
 					//それ以上左に行けないようにする
-					PosX = 16.0f;
+					PosX = 32.0f;
 				}
 			}
 		}
 
-		void DrawPlayer()	//プレイヤー描画処理
+		void ChangePlayerImage()	//プレイヤーの画像を変える
 		{
-			DrawRotaGraph((int)PosX, (int)PosY, 1.0f, 0.0f, PlayerHandle, true);
-		}
+			AnimeFrame++;	//再生時間をカウント
+			if (AnimeFrame >= FRAME_TIME / 2)	//一定の時間に達したら
+			{
+				AnimeFrame = 0;		//再生時間をリセット
 
-		void FinPlayer()	//プレイヤーの後処理
-		{
-			//画像を破棄
-			DeleteGraph(PlayerHandle);
+				if (!isReverse)		//逆再生フラグがfalseなら
+				{
+					if (HandleIndex == 9)	//最後の画像なら
+					{
+						isReverse = true;	//逆再生にする
+						HandleIndex = 8;	//1つ前の画像にする
+					}
+
+					else	//最後の画像でないなら
+					{
+						HandleIndex++;	//次の画像に切り替える
+					}
+				}
+
+				else
+				{
+					if (HandleIndex == 0)	//最初の画像なら
+					{
+						isReverse = false;	//通常再生にする
+						HandleIndex = 1;	//1つ次の画像にする
+					}
+
+					else	//最初の画像でないなら
+					{
+						HandleIndex--;	//前の画像に切り替える
+					}
+				}
+			}
 		}
 };
 
