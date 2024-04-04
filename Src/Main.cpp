acc309b8ed235c066de8dbd9918bc65c8a0a6679
ster1271 <144640199+ster1271@users.noreply.github.com>
2024-04-04 09:46:36 +0900

    Update Main.cpp

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -4,11 +4,8 @@
 #include "Common.h"
 #include "Scene/Scene.h"
 #include "Scene/SceneTitle.h"
-<<<<<<< Updated upstream
 #include "Scene/SceneClear.h"
-=======
 #include "Scene/SceneGameOver.h"
->>>>>>> Stashed changes
 
 // define
 #define	SCREEN_SIZE_X	1280	// X方向の画面サイズを指定
