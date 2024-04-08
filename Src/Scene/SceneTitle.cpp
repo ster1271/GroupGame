b26b0f82398b5bf82aa5f3ae584755a5c08d70cc
ster1271 <144640199+ster1271@users.noreply.github.com>
2024-04-08 19:07:56 +0900

    Title 画像と処理の追加

--- Src/Scene/SceneTitle.cpp
+++ Src/Scene/SceneTitle.cpp
@@ -5,12 +5,17 @@
 //タイトル背景画像のハンドル
 int BgTitleHandle;
 
+//矢印画像ハンドル
+int S_Handle;
 
 //タイトル初期化
 void InitTitle()
 {
 	//タイトル背景画像の初期化
 	BgTitleHandle = LoadGraph(BG_TITLE_PATH);
+
+	//矢印画像の初期化
+	S_Handle = LoadGraph(SELECT_PATH);
 
 	//タイトルループへ
 	g_CurrentSceneID = SCENE_ID_LOOP_TITLE;
@@ -33,6 +38,10 @@ void DrawTitle()
 	//タイトル背景画像の描画
 	DrawGraph(0, 0, BgTitleHandle, true);
 
+	//矢印画像の描画
+	DrawRotaGraph(430, 630, 2.0f, 0.0f, S_Handle, true, false, false);
+
+
 }
 
 //タイトル後処理
@@ -40,6 +49,9 @@ void FinTitle()
 {
 	//タイトル背景画像の破棄
 	DeleteGraph(BgTitleHandle);
+
+	//矢印画像の破棄
+	DeleteGraph(S_Handle);
 
 	//タイトルの後はプレイへ
 	g_CurrentSceneID = SCENE_ID_INIT_PLAY;
--- Src/Scene/SceneTitle.h
+++ Src/Scene/SceneTitle.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define BG_TITLE_PATH	("Data/TitleImage/BgTitle.png")		//タイトル背景画像のパス
+#define SELECT_PATH		("Data/ClearImage/Select.png")		//矢印画像パス
 
 //タイトル初期化
 void InitTitle();
