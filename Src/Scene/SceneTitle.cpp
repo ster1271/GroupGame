f487728b5271c37907139326d63ca2be60d43281
ster1271 <144640199+ster1271@users.noreply.github.com>
2024-04-08 19:02:02 +0900

    タイトルの変更

--- Src/Scene/SceneTitle.cpp
+++ Src/Scene/SceneTitle.cpp
@@ -5,23 +5,12 @@
 //タイトル背景画像のハンドル
 int BgTitleHandle;
 
-//タイトル文字画像のハンドル
-int TitleTextHandle;
-
-//スタート文字画像のハンドル
-int StartTextHandle;
 
 //タイトル初期化
 void InitTitle()
 {
 	//タイトル背景画像の初期化
 	BgTitleHandle = LoadGraph(BG_TITLE_PATH);
-
-	//タイトル文字画像の初期化
-	TitleTextHandle = LoadGraph(TITLE_TEXT_PATH);
-
-	//スタート文字画像の初期化
-	StartTextHandle = LoadGraph(START_TEXT_PATH);
 
 	//タイトルループへ
 	g_CurrentSceneID = SCENE_ID_LOOP_TITLE;
@@ -44,11 +33,6 @@ void DrawTitle()
 	//タイトル背景画像の描画
 	DrawGraph(0, 0, BgTitleHandle, true);
 
-	//タイトル文字画像の描画
-	DrawRotaGraph(320, 150, 1.0f, 0.0f, TitleTextHandle, true);
-
-	//スタート文字画像の描画
-	DrawRotaGraph(320, 400, 1.0f, 0.0f, StartTextHandle, true);
 }
 
 //タイトル後処理
@@ -56,12 +40,6 @@ void FinTitle()
 {
 	//タイトル背景画像の破棄
 	DeleteGraph(BgTitleHandle);
-
-	//タイトル文字画像の破棄
-	DeleteGraph(TitleTextHandle);
-
-	//スタート文字画像の破棄
-	DeleteGraph(StartTextHandle);
 
 	//タイトルの後はプレイへ
 	g_CurrentSceneID = SCENE_ID_INIT_PLAY;
--- Src/Scene/SceneTitle.h
+++ Src/Scene/SceneTitle.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #define BG_TITLE_PATH	("Data/TitleImage/BgTitle.png")		//タイトル背景画像のパス
-#define TITLE_TEXT_PATH	("Data/TitleImage/TitleText.png")	//タイトル文字画像のパス
-#define START_TEXT_PATH	("Data/TitleImage/PressEnter.png")	//スタート文字画像のパス
 
 //タイトル初期化
 void InitTitle();
