8fe13921da5926f09f1a6f599cbec80a6f181d56
ster1271 <144640199+ster1271@users.noreply.github.com>
2024-04-09 10:56:12 +0900

    タイトルの追加

--- Src/Scene/SceneTitle.cpp
+++ Src/Scene/SceneTitle.cpp
@@ -5,6 +5,9 @@
 //タイトル背景画像のハンドル
 int BgTitleHandle;
 
+//タイトル画像ハンドル
+int TitleHundle;
+
 //BGMのハンドル
 int Title_Bgm;
 
@@ -13,6 +16,9 @@ void InitTitle()
 {
 	//タイトル背景画像の初期化
 	BgTitleHandle = LoadGraph(BG_TITLE_PATH);
+
+	//タイトル画像読み込み
+	TitleHundle = LoadGraph(TITLE_PATH);
 
 	//BGM読み込み
 	Title_Bgm = LoadSoundMem(TITLE_BGM_PATH);
@@ -40,6 +46,9 @@ void DrawTitle()
 {
 	//タイトル背景画像の描画
 	DrawGraph(0, 0, BgTitleHandle, true);
+
+	//タイトル画像の描画
+	DrawGraph(250, 0, TitleHundle, true);
 }
 
 //タイトル後処理
--- Src/Scene/SceneTitle.h
+++ Src/Scene/SceneTitle.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define BG_TITLE_PATH	("Data/TitleImage/BgTitle.png")		//タイトル背景画像のパス
+#define TITLE_PATH		("Data/TitleImage/title.png")		//タイトル画像パス
 #define TITLE_BGM_PATH	("Data/Sounds/Music/Title/Title.mp3")	//タイトルBGMのパス
 
 //タイトル初期化
